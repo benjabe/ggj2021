fd5edd51d5b4da48eed86828d1d86b7877ac6af1
Jigurd <sigurd.stapnes@gmail.com>
2021-01-30 23:40:44 +0100

    merge

---
+++ Assets/Scripts/FastForward.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FastForward : MonoBehaviour{
+    [SerializeField] private Button[] buttons;
+    [SerializeField] private float[] speed;
+    public void Start(){
+        for(var i = 0; i<buttons.Length; i++){
+            var j = i;
+            buttons[i].onClick.AddListener(() => Time.timeScale = speed[j]);
+        }
+    }
+}
